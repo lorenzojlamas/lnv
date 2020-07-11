7139860774b482fd5a7242b8ef1c3b4ce36ca4db
Lorenzo José Lamas <lorenzojlamas@gmail.com>
2020-07-11 14:17:06 -0300

    feat: exactas juega inicial (#8)
    * feat: initial Exactas juega page
    
    Co-authored-by: Lorenzo Lamas <lorenzolamas@MacBook-Pro-de-Lorenzo.local>

---
+++ src/components/Titles/TitleWithOuthText.tsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import React from "react";
+
+import Container from 'react-bootstrap/Container';
+import Badge from 'react-bootstrap/Badge';
+
+
+export interface ITitleForPagesWithIconProps {
+    title: string;
+    icon: JSX.Element
+}
+
+export class TitleForPagesWithIcon extends Component<ITitleForPagesWithIconProps> {
+    render() {
+        const {title, icon} = this.props;
+        return (
+        <Container fluid className="content-md-center" style={{ textAlign: "center" }}>
+                <h1>{icon}{' '}<Badge variant="dark">{title}</Badge></h1>
+        </Container>
+        );
+    }
+}
